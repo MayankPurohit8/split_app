bea89a4bab4db7ec200baa31c2ea72418e9b737f
MayankPurohit8 <mayankpurohit888@gmail.com>
2026-01-28 11:38:53 +0530

    integration resolve

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const conn = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/split_db");
+    await mongoose.connect(`${process.env.MONGODB_URI}split_db`);
     console.log("connection to database established");
   } catch (err) {
     console.log(err);
